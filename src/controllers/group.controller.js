78dec07e9263494959fc3296c43d144c2b602614
ahujavishal473 <vishalahuja473@gmail.com>
2025-05-07 18:05:34 +0530

    update

--- src/controllers/group.controller.js
+++ src/controllers/group.controller.js
@@ -106,7 +106,7 @@ export const getUserGroup=async(req,res)=>{
         if(!group){
             return res.json({message:'You not Joined in Group'})
         }
-        res.status(200).json({id:group._id},{group})
+        res.status(200).json({group})
     }
     catch(err){
         res.status(500).json({message:err.message})
